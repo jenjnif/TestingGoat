cec4eaea65f678b60fe2f36e5b6e55b6348f188a
jenjnif <jenjnif@hotmail.com>
2018-08-08 15:41:53 +0100

    using unittest module to tidy up browser window and test

--- functional_tests.py
+++ functional_tests.py
@@ -1,33 +1,45 @@
 from selenium import webdriver
 import unittest
 
-browser = webdriver.Firefox()
 
-# Adam Ondra has heard about a cool new climbing to-do app
-# He goes to check out the homepage
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-# He notices the page title and header mention to-do lists
-assert 'To-Do' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-# He is invited to enter a to-do item straight away
+    def tearDown(self):
+        self.browser.quit()
 
-# He types "Climb a 9d" into a text box (Adam's speciallity is sport climbing)
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Adam Ondra has heard about a cool new climbing to-do app
+        # He goes to check out the homepage
+        self.browser.get('http://localhost:8000')
 
-# When he hits enter, the page updates, and now the page lists
-# "1: Climb a 9d" as an item in a to-do list
+        # He notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
 
-# There is still a text box inviting him to add another item
-# He enters "Hang out with Alex Megos in Frankenjura" (Adam is very social)
+        # He is invited to enter a to-do item straight away
 
-# The page udpates again, and now shows both items on his list
+        # He types "Climb a 9d" into a text box (Adam's speciallity is sport
+        # climbing)
 
-# Adam wonders if the site will remember his list
-# Then he sees that the site has generated a unique URL for him -- there is
-# some explanatory text to that effect
+        # When he hits enter, the page updates, and now the page
+        # lists: "1: Climb a 9d" as an item in a to-do list
 
-# He visits that URL - his to-do list is still there
+        # There is still a text box inviting him to add another item. He enters
+        # "Hang out with Alex Megos in Frankenjura" (Adam is very social)
 
-# Satisfied, he goes back to training
+        # The page udpates again, and now shows both items on his list
 
-browser.quit()
+        # Adam wonders if the site will remember his list. Then he sees that
+        # the site has generated a unique URL for him -- there is some
+        # explanatory text to that effect
+
+        # He visits that URL - his to-do list is still there
+
+        # Satisfied, he goes back to training
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
