89bbcdefc21e6bf6b41dccbc45c03f82b297e51e
jenjnif <jenjnif@hotmail.com>
2018-08-22 15:33:01 +0100

    remove 'ignore warnings' and add implicitly_wait

--- functional_tests.py
+++ functional_tests.py
@@ -6,6 +6,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
@@ -42,4 +43,4 @@ class NewVisitorTest(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    unittest.main(warnings='ignore')
+    unittest.main()
